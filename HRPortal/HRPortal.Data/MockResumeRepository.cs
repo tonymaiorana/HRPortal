689f09741d556ff17fea5b24f3bf48cb92adb065
Eleanor Miller <MillerLElle@gmail.com>
2016-03-22 16:58:21 -0400

    ETT-2 set up mock repo

--- HRPortal/HRPortal.Data/MockResumeRepository.cs
+++ HRPortal/HRPortal.Data/MockResumeRepository.cs
@@ -9,29 +9,64 @@ namespace HRPortal.Data
 {
     public class MockResumeRepository : IResumeRepository
     {
+        private static List<Resume> _resumes = new List<Resume>();
+
+        public MockResumeRepository()
+        {
+            if (!_resumes.Any())
+            {
+                _resumes.AddRange(new List<Resume>()
+                {
+                    new Resume
+                    {
+                        ResumeID = 1,
+                        FirstName = "Goo",
+                        LastName = "Frob",
+                        DateCreated = DateTime.Today,
+                        DateUpdated = DateTime.Today,
+                        ApplyingPosition = null,
+                        ResumeFile = null
+                    },
+                    new Resume
+                     {
+                        ResumeID = 2,
+                        FirstName = "Doooo",
+                        LastName = "Man",
+                        DateCreated = DateTime.Today,
+                        DateUpdated = DateTime.Today,
+                        ApplyingPosition = null,
+                        ResumeFile = null
+                    }
+                });
+            }
+        }
+
         public List<Resume> GetAllResumes()
         {
-            throw new NotImplementedException();
+            return _resumes;
         }
 
         public void Add(Resume newResume)
         {
-            throw new NotImplementedException();
+            newResume.ResumeID = (_resumes.Any()) ? _resumes.Max(r => r.ResumeID) + 1 : 1;
+
+            _resumes.Add(newResume);
         }
 
         public void Delete(int resumeID)
         {
-            throw new NotImplementedException();
+            _resumes.RemoveAll(r => r.ResumeID == resumeID);
         }
 
         public void Edit(Resume resume)
         {
-            throw new NotImplementedException();
+            Delete(resume.ResumeID);
+            _resumes.Add(resume);
         }
 
         public Resume GetByID(int resumeID)
         {
-            throw new NotImplementedException();
+            return _resumes.FirstOrDefault((r => r.ResumeID == resumeID));
         }
     }
 }
