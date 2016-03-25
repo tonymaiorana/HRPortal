f1b2f873bc50a4f1b3dacf283f49aed3c325c6f3
Timothy Mueller <TJMueller@DonorSnap.com>
2016-03-25 16:41:22 -0400

    ETT-6 home screen and general layout

--- HRPortal/HRPortal.Models/Resume.cs
+++ HRPortal/HRPortal.Models/Resume.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HRPortal.Models
 {
-    public class Resume //todo: :IValidatableObject
+    public class Resume : IValidatableObject
     {
         public Person Applicant { get; set; }
+
         //[DataType(DataType.Date)]
         public DateTime DateCreated { get; set; }
+
         //[DataType(DataType.Date)]
         public DateTime DateUpdated { get; set; }
+
         public Position ApplyingPosition { get; set; }
         public object ResumeFile { get; set; }
         public int ResumeID { get; set; }
         public Education Education { get; set; }
         public JobExperience JobExperience { get; set; }
         //todo: refrences??
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
