a916a93ff9de502ac2a4818bff0ff7d8c609c9ea
Timothy Mueller <TJMueller@DonorSnap.com>
2016-03-24 21:12:47 -0400

    ETT-6 started a partial code for a popout file submission

---
+++ HRPortal/HRPortal.UI/Models/SubmitResumeVM.cs
@@ -0,0 +1,15 @@
+﻿using HRPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRPortal.UI.Models
+{
+    public class SubmitResumeVM
+    {
+        public Resume Resume { get; set; }
+        public List<Position> AvailiblePositions { get; set; }
+        public List<Education> Educations { get; set; }
+    }
+}
