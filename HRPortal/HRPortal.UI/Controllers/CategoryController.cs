9069d946d6f123faa3968d009ae950fd47e56c5e
Eleanor Miller <MillerLElle@gmail.com>
2016-03-29 17:23:46 -0400

    ETT-12 started policies views

---
+++ HRPortal/HRPortal.UI/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRPortal.Models;
+
+namespace HRPortal.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            //needs finished
+            return View( /*need to finish*/);
+        }
+    }
+}
