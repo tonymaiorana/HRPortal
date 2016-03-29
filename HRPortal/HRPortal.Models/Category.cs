c5372567b56b938fe8134b18a6b75727cacb255a
Timothy Mueller <TJMueller@DonorSnap.com>
2016-03-29 14:34:28 -0400

    ETT-10 created policy and category model

---
+++ HRPortal/HRPortal.Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRPortal.Models
+{
+    public class Category
+    {
+        public string CategoryTitle { get; set; }
+    }
+}
