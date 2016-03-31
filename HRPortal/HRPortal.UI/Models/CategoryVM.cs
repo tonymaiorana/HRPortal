43239da18fc4ba0e509cfcbd9720e987f6f9b31e
Eleanor Miller <MillerLElle@gmail.com>
2016-03-31 17:08:02 -0400

    ETT-13 still working on views for catigory

---
+++ HRPortal/HRPortal.UI/Models/CategoryVM.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using HRPortal.Models;
+
+namespace HRPortal.UI.Models
+{
+    public class CategoryVM
+    {
+        public CategoryVM(List<Category> getAllCategories)
+        {
+            
+        }
+    }
+}
