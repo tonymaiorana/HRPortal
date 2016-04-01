14d54a0c21c22251342514e9ed3ca7ea1c45acb0
Timothy Mueller <TJMueller@DonorSnap.com>
2016-03-31 21:54:27 -0400

    ETT-11 Got create policy working as well as policy index

--- HRPortal/HRPortal.Models/Policy.cs
+++ HRPortal/HRPortal.Models/Policy.cs
@@ -9,18 +9,20 @@ namespace HRPortal.Models
     public class Policy
     {
         public Category Category { get; set; }
+        public int CategoryID { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public int PolicyID { get; set; }
 
         public Policy()
         {
-            Category = new Category { CategoryID = 0, CategoryTitle = "Default" };
+            CategoryID = 0;
         }
 
         public Policy(Category c)
         {
             Category = c;
+            CategoryID = c.CategoryID;
         }
     }
 }
---
+++ HRPortal/HRPortal.UI/Models/PolicyVM.cs
@@ -0,0 +1,27 @@
+﻿using HRPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRPortal.UI.Models
+{
+    public class PolicyVM
+    {
+        public Policy Policy { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+
+        public PolicyVM(List<Category> categories)
+        {
+            Categories = new List<SelectListItem>();
+            foreach (Category category in categories)
+            {
+                SelectListItem categorySelectListItem = new SelectListItem() { Text = category.CategoryTitle, Value = category.CategoryID.ToString() };
+                Categories.Add(categorySelectListItem);
+            }
+        }
+    }
+}
