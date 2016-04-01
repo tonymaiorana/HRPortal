f815c0fb3c0c5df9206bd19056e2579b238171a4
Eleanor Miller <MillerLElle@gmail.com>
2016-04-01 14:10:50 -0400

    ETT-2 ETT-4 set up test for repo

---
+++ HRPortal/HRPortal.Tests/RepoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRPortal.Data;
+using NUnit.Framework;
+
+namespace HRPortal.Tests
+{
+    [TestFixture]
+    public RepoTests
+    {
+        [Test]
+        public void CanLoadResumes()
+        {
+            var repo = new ResumeRepository();
+
+        
+        }
+    }
+}
